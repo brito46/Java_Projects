5c6b08f9f1d20c1ae85bd432f831dea1d2c56971
Jonathan Brito <britoj2@tcnj.edu>
2020-05-23 15:07:11 -0400

    Initial Commit

---
+++ FileReader9000.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+//your next task is to find out how many times a file says regardless
+//and then the other tasks is to replace the word regardless with however
+public class FileReader9000 {
+     public static void main(String []args){
+
+     	//the file we wanted to read was http://textfiles.com/100/captmidn.txt
+
+     	File file = new File("\Users\jbrit\Documents\captmidn.txt"); //about to open a new file; 
+     	                    //we pass the path of the file as a string; i put the location of where my file can be found
+
+     	Scanner scan = new Scanner(file); //scanning file; but we usually make it scan our input
+
+     	String fileContent = "This is an updated file revise by JB\n";
+
+     	while(scan.hasNextLine()){ //it'll stay in while loop until it scans every line
+     		// System.out.println(scan.nextLine()); //scan the first line of file and printing it out and move to next line
+     		                                     //this loop will print every line of the file
+
+     		fileContent = fileContent.concat(scan.nextLine() + "\n"); //exporting the file to a new one by storing it to string
+     		//concat is almost as same as append by adding string to the end of string, but append allows string characters to be added to end as well
+     	}
+
+     	FileWriter writer = new FileWriter("\Users\jbrit\Documents\updatedFile.txt"); //adding the path of the new file;also called new file as "updatedfile"
+     	writer.write(fileContent);                                                    //this new file was created by the java program
+     	writer.close(); 
+     }
+ }
---
+++ TicTacToe.java
@@ -0,0 +1,121 @@
+import java.util.*;
+
+
+public class TicTacToe {
+	static ArrayList<Integer> playerPos = new ArrayList<>(); //static so it can be accessed by any function
+    static ArrayList<Integer> cpuPos = new ArrayList<>(); 
+
+     public static void main(String []args){
+
+     	 char [][] game = {{' ', '|', ' ', '|', ' '}, //how to create a multi dim array for any kind(int and str too)
+     	 				   {'-', '+', '-', '+', '-'},
+     	 				   {' ', '|', ' ', '|', ' '}, 
+     	                   {'-', '+', '-', '+', '-'},
+     	                   {' ', '|', ' ', '|', ' '}};
+     	while(true){
+     		printBoard(game);  //showing where cpu put their piece
+     		Scanner scan = new Scanner(System.in);   //you have to create a new input everytime for a new player input
+     		System.out.println("Enter position from 1-9:");
+     		int pos = scan.nextInt();
+     		while(playerPos.contains(pos) || cpuPos.contains(pos)){
+     			System.out.println("position taken! try another one:");
+     			pos = scan.nextInt(); //try a new input for a value that isnt taken
+     		}
+     		playerPos.add(pos);
+			move(game, pos, "Player");
+			String ans = checkWinner();
+			printBoard(game); //seeing where i put my piece after i've gone
+     		System.out.println(ans);
+     		if(ans.length() > 0) break; //game ended so get out of forver loop
+
+     		Random rand = new Random();
+     		int rando = rand.nextInt(9) + 1; //prints out a random number from 1-9
+     		while(playerPos.contains(rando) || cpuPos.contains(rando)){
+     			System.out.println("position taken! try another one:");
+     			rando = rand.nextInt(9) + 1;//try a new input for a value that isnt taken
+     		}
+     		cpuPos.add(rando);
+     		move(game, rando, "cpu");
+
+     		System.out.println(ans);
+     		if(ans.length() > 0) break; //game ended so get out of forver loop
+     	}
+     }
+     public static void printBoard(char [][] g){
+
+        for(int i = 0; i < g.length; i++){
+     		for(int j = 0; j < g[0].length; j++){
+
+     			System.out.print(g[i][j]); //printing out every column in row
+     		}
+     		System.out.println();//so the new row can be printed below the prev
+     	}
+     }
+     public static void move(char[][] game, int x, String user){
+     		char symbol = 'X'; //assuming it is Player's turn
+     		if(user.equals("cpu")) symbol = 'O'; //switching symbols depedning on whos turn
+
+          	switch(x) {
+
+     		case 1 :  //if pos == 1;
+     			game[0][0] = symbol;
+     			break;
+     		case 2 :  
+     			game[0][2] = symbol;
+     			break;
+     		case 3 :  
+     			game[0][4] = symbol;
+     			break;
+     		case 4 :  
+     			game[2][0] = symbol;
+     			break;
+     		case 5 :  
+     			game[2][2] = symbol;
+     			break;
+     		case 6 :  
+     			game[2][4] = symbol;
+     			break;
+     		case 7 :  
+     			game[4][0] = symbol;
+     			break;
+     		case 8 :  
+     			game[4][2] = symbol;
+     			break;
+     		case 9 :  
+     			game[4][4] = symbol;
+     			break;
+     		default:
+     		 	break;
+     	}
+     }
+     public static String checkWinner() {
+     	List topRow = Arrays.asList(1, 2, 3); //the first three elements are the first three columns in first row
+     	List midRow = Arrays.asList(4, 5, 6);
+     	List botRow = Arrays.asList(7, 8, 9);
+     	List leftCol = Arrays.asList(1, 4, 7);
+     	List midCol = Arrays.asList(2, 5, 8);
+     	List rightCol = Arrays.asList(3, 6, 9);
+     	List diag1 = Arrays.asList(1, 5, 9);
+     	List diag2 = Arrays.asList(3, 5, 7);
+
+     	ArrayList<List> winning = new ArrayList<>();
+     	winning.add(topRow);
+     	winning.add(midRow);
+     	winning.add(botRow);
+     	winning.add(leftCol);
+     	winning.add(midCol);
+     	winning.add(rightCol);
+     	winning.add(diag1);
+     	winning.add(diag2);
+
+     	for(List l : winning){ //this is going through every list in winning, so basically winning.get(i)
+     		if(playerPos.containsAll(l)) return "Player wins";  //checking every list in winning
+
+     		else if(cpuPos.containsAll(l)) return "cpu wins";
+
+     		else if(playerPos.size() + cpuPos.size() == 9) return "Tie"; //board is full and nobody won
+
+     	}
+     	return ""; //if there is no winners or tie at the moment
+     }
+ }
